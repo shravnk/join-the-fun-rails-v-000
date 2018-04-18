5ac5336a104bd1fe92d247e9ac259dda70cfa86d
Brendan O'Dea <brendan.ode4@gmail.com>
2018-04-18 05:41:38 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,26 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180418052955) do
+ActiveRecord::Schema.define(version: 20180418054055) do
 
   create_table "passengers", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "passengers_rides", id: false, force: :cascade do |t|
-    t.integer "ride_id",      null: false
-    t.integer "passenger_id", null: false
-  end
-
   create_table "rides", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "rides_taxis", id: false, force: :cascade do |t|
-    t.integer "ride_id", null: false
-    t.integer "taxi_id", null: false
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.integer  "passenger_id"
+    t.integer  "taxi_id"
   end
 
   create_table "taxis", force: :cascade do |t|
